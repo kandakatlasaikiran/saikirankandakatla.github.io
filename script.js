c6fea5a4d7ffed1c82b8d3b053f4723e1fe5c3e7
kandakatla saikiran <saikiran3ksk@gmail.com>
2024-02-08 18:09:52 +0530

    Portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed((".skills"),{
+    strings : ["Python Fullstack Developer","Ui Designer"],
+    typeSpeed : 100,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+})
